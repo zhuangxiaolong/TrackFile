efae1407360eb3bd87162538203eb4a823cb6cb3
colin <colin.zhuang@gzmediaclick.com>
2018-12-11 11:18:25 +0800

    添加项目文件。

---
+++ TrackFile/FrmMain.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace TrackFile
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDevice = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFileDir = new System.Windows.Forms.TextBox();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.plog = new System.Windows.Forms.Panel();
+            this.plog.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "设备ID";
+            // 
+            // txtDevice
+            // 
+            this.txtDevice.Location = new System.Drawing.Point(91, 18);
+            this.txtDevice.Name = "txtDevice";
+            this.txtDevice.Size = new System.Drawing.Size(318, 21);
+            this.txtDevice.TabIndex = 1;
+            this.txtDevice.Text = "ABCD123455";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "图片目录";
+            // 
+            // txtFileDir
+            // 
+            this.txtFileDir.Location = new System.Drawing.Point(91, 56);
+            this.txtFileDir.Name = "txtFileDir";
+            this.txtFileDir.Size = new System.Drawing.Size(318, 21);
+            this.txtFileDir.TabIndex = 3;
+            this.txtFileDir.Text = "C:\\Users\\Administrator\\Pictures";
+            // 
+            // btnFile
+            // 
+            this.btnFile.Location = new System.Drawing.Point(416, 56);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(75, 23);
+            this.btnFile.TabIndex = 4;
+            this.btnFile.Text = "浏览";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(91, 137);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "启动";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(334, 137);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(91, 96);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(318, 21);
+            this.txtUrl.TabIndex = 8;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLog.Location = new System.Drawing.Point(0, 0);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtLog.Size = new System.Drawing.Size(575, 194);
+            this.txtLog.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "上传图片URL";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "日志输出";
+            // 
+            // plog
+            // 
+            this.plog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.plog.AutoScroll = true;
+            this.plog.Controls.Add(this.txtLog);
+            this.plog.Location = new System.Drawing.Point(91, 177);
+            this.plog.Name = "plog";
+            this.plog.Size = new System.Drawing.Size(575, 194);
+            this.plog.TabIndex = 11;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 375);
+            this.Controls.Add(this.plog);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnFile);
+            this.Controls.Add(this.txtFileDir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDevice);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Track File";
+            this.plog.ResumeLayout(false);
+            this.plog.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDevice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFileDir;
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel plog;
+    }
+}
+
---
+++ TrackFile/FrmMain.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TrackFile
+{
+    public partial class FrmMain : Form
+    {
+        private Timer _timer;
+        private List<string> _lstFile;
+        public FrmMain()
+        {
+            InitializeComponent();
+            Load += Frm_Load;
+        }
+
+        protected void Frm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                _timer = new Timer
+                {
+                    Interval = 3*1000
+                };
+                _timer.Tick += TimerTrackFile;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(txtFileDir.Text.Trim()))
+                    throw new ApplicationException("图片目录不能为空");
+               // if (string.IsNullOrEmpty(txtUrl.Text.Trim()))
+                 //   throw new ApplicationException("上传图片URL不能为空");
+                var folderResult = Directory.Exists(txtFileDir.Text.Trim());
+                if (!folderResult)
+                    throw new ApplicationException("图片目录不正确");
+
+                LoadFolderFile();
+                  _timer.Start();
+               // TimerTrackFile(null, null);
+                StartStatus();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void LoadFolderFile()
+        {
+            _lstFile = new List<string>();
+            var lst = Directory.GetFiles(txtFileDir.Text.Trim());
+            foreach (var s in lst)
+            {
+                try
+                {
+                    if (!File.Exists(s))
+                        continue;
+                    _lstFile.Add(s);
+                    AddLog(string.Format("当前文件：{0}",s));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            AddLog(string.Format("当前文件一共：{0}", _lstFile.Count));
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            _timer.Stop();
+            StopStatus();
+        }
+
+        private void StartStatus()
+        {
+            btnStart.Enabled = false;
+            txtFileDir.Enabled = false;
+            txtUrl.Enabled = false;
+            txtDevice.Enabled = false;
+            btnFile.Enabled = false;
+            AddLog("开始监听");
+        }
+        private void StopStatus()
+        {
+            btnStart.Enabled = true;
+            txtFileDir.Enabled = true;
+            txtUrl.Enabled = true;
+            txtDevice.Enabled = true;
+            btnFile.Enabled = true;
+            AddLog("停止监听");
+        }
+
+        private void TimerTrackFile(object sender, EventArgs e)
+        {
+            try
+            {
+                var lst = Directory.GetFiles(txtFileDir.Text.Trim());
+                for (int i = lst.Length-1; i >0; i--)
+                {
+                    var filename = lst[i];
+                    if (!File.Exists(filename))
+                        continue;
+                    if (_lstFile.Contains(filename))
+                        continue;
+                    //监听到新文件
+                    TrackNewFile(filename);
+                }
+
+            }
+            catch (Exception err)
+            {
+                AddLog(err.Message);
+            }
+        }
+
+        private void TrackNewFile(string filename)
+        {
+            //移到子目录
+            var newfileFullname = MoveNewFile(filename);
+            //上传到URL
+            UploadNewFile(newfileFullname);
+            //写日志
+            var file=new FileInfo(newfileFullname);
+            AddLog(string.Format("完成文件：{0} 的上传并移到 {1} 子目录", file.Name, file.Directory));
+        }
+
+        private void UploadNewFile(string filename)
+        {
+             if (string.IsNullOrEmpty(txtUrl.Text.Trim()))
+                return;
+            //todo
+        }
+        private string MoveNewFile(string filename)
+        {
+            var file = new FileInfo(filename);
+            var folder = DateTime.Now.ToString("yyyy-MM-dd");
+            var fileFolder=Path.Combine(file.Directory.FullName, folder);
+            //检查目录是否已存在
+            if (!Directory.Exists(fileFolder))
+                Directory.CreateDirectory(fileFolder);
+
+            var newfileFullname = Path.Combine(fileFolder, file.Name);
+            //检查文件是否已存在
+            if(File.Exists(newfileFullname))
+                File.Delete(newfileFullname);
+
+            File.Move(filename, newfileFullname);
+            return newfileFullname;
+        }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var folder=new FolderBrowserDialog();
+                var result=folder.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    txtFileDir.Text = folder.SelectedPath;
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private int logNum = 0;
+        private void AddLog(string log)
+        {
+            if (logNum == 1000)
+                txtLog.Text = string.Empty;
+
+            if (string.IsNullOrWhiteSpace(txtLog.Text))
+            {
+                txtLog.Text =string.Format("{0} {1}",DateTime.Now.ToString("yyyy-MM-dd HH:mm:sss"),log);
+                return;
+            }
+            txtLog.Text = string.Format("{0} {1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:sss"), log) 
+                + "\r\n" + txtLog.Text;
+
+            logNum++;
+        }
+    }
+}
